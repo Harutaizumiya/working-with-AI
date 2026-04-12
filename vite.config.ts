868038688abb077bc488cce8c68f68b9a2580064
Harutaizumiya <v2920094235@outlook.com>
2026-04-12 22:06:22 +0800

    feat：加入了术语解释页

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite-plus";
+import { CodeInspectorPlugin } from 'code-inspector-plugin';
 
 export default defineConfig({
   lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [
+    CodeInspectorPlugin({
+      bundler: 'vite',
+    }),
+    ],
 });
